dbceac5241dc358d08b7d83f7384ae9afa241814
parimal mundhava <parimalbharavad8182@gmail.com>
2026-08-15 21:48:58 +0530

    Time: 4 ms (30.66%) | Memory: 8.6 MB (36.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -6,18 +6,11 @@ public:
         }
 
         int reversedHalf = 0;
-        
-        // Step 2: Reverse the second half of the number
-        // We know we've reached the middle of the number when x <= reversedHalf
+
         while (x > reversedHalf) {
             reversedHalf = reversedHalf * 10 + x % 10;
             x /= 10;
         }
-
-        // Step 3: Check for equality
-        // If the number has an even number of digits, x == reversedHalf.
-        // If it has an odd number of digits, we drop the middle digit using reversedHalf / 10.
-        // (e.g., if input is 12321, at the end x = 12 and reversedHalf = 123)
         return x == reversedHalf || x == reversedHalf / 10;    
 
     }
