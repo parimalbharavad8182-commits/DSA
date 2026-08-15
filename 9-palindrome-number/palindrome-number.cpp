7fe0a22cdf4bb4eadd3a41abdd08943817fc1d8c
parimal mundhava <parimalbharavad8182@gmail.com>
2026-08-15 21:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-    if (x < 0 || (x % 10 == 0 && x != 0)) {
+        
+        if (x < 0) {
             return false;
         }
 
-        int reversedHalf = 0;
+        long long n = x;
+        long long original = n;
+        long long r = 0;
 
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
+        while (n > 0) {
+            r = r * 10 + n % 10;
+            n = n / 10;
         }
-        return x == reversedHalf || x == reversedHalf / 10;    
 
+        if (r == original) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 };
